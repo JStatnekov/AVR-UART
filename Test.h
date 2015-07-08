3ff712bd4b753b05d2a139f2412fae1a95e40ccc
jstatnekov <jacob.statnekov@gmail.com>
2015-07-08 14:19:49 -0700

    updating test file for buffered reads

--- Test.h
+++ Test.h
@@ -4,14 +4,15 @@
 #include "../LCDControl.h"
 #include "uart.h"
 
-int TestUART(void){
+int TestUART(){
 	
 	LCDInit();
-	UARTInit();
+	UARTInitBlocking();
 	DDRD |= (1<<PORTD3);
 	
-	UARTSendStr("Hello\n");
+	UARTSendStrBlocking("Hello\n");
 	LCDWriteStr("waiting...",0);
+	uint8_t firstTimeThrough = 1;
 	
 	uint8_t columnPosition=0, rowPosition=0;
 	char input;
@@ -24,12 +25,76 @@ int TestUART(void){
 			columnPosition = 0;
 		}
 		
-		input = UARTGetChar();
-		PORTD ^= (1<<PORTD3);//we flash the light every time we recieve a character
+		input = UARTGetCharBlocking();
+		PORTD ^= (1<<PORTD3);//we flash the light every time we receive a character
 		
-		UARTSendStr("You wrote ");
-		UARTSendChar(input);
-		UARTSendChar('\n');
+		UARTSendStrBlocking("You wrote ");
+		UARTSendCharBlocking(input);
+		UARTSendCharBlocking('\n');
+		if(firstTimeThrough)
+		{
+			firstTimeThrough = 0;
+			LCDClear();
+		}
+		
 		LCDWriteChar(input,0,1);		
+	}
+};
+
+
+
+int TestUARTAsynch(){
+	
+	LCDInit();
+	UARTAsynchInit();
+	
+	DDRD |= (1<<PORTD3);
+	PORTD ^= (1<<PORTD3);
+	
+	UARTSendStrBlocking("Hello\n");
+	
+	uint8_t firstTimeThrough = 1;
+	
+	uint8_t columnPosition=0, rowPosition=0;
+	char input;
+	while(1) {
+			
+		char * buffer = GetBuffer();
+			
+		LCDSetTextCursorPosition(0, 0);	
+		LCDWriteStr("BuffAddr ",0);
+		LCDWriteSmallNumberAsString((uintptr_t)buffer);
+		LCDSetTextCursorPosition(1, 0);
+		LCDWriteStr("ReadAddr ",0);	
+		LCDWriteSmallNumberAsString((uintptr_t)GetLastReadCharPtr());
+		LCDSetTextCursorPosition(2, 0);
+		LCDWriteStr("RecAddr  ",0);
+		LCDWriteSmallNumberAsString((uintptr_t)GetLastRecievedCharPtr());
+		
+		LCDSetTextCursorPosition(3, 0);
+		for(int i=0; i <UARTBufferSize;i++){
+			LCDWriteChar(buffer[i],0,1);
+			if(i % 16 == 0)//16 chars per line
+			{
+				LCDSetTextCursorPosition(3 + i/16,0);
+			}
+		}
+		
+		//we wait until the buffer is full		
+		if(UARTIsBufferFull())
+		{
+			UARTSendStrBlocking("You wrote ");
+			do{
+				input = UARTAsynchReadFromBuffer();
+				
+				UARTSendCharBlocking(input);
+				if(firstTimeThrough)
+				{
+					firstTimeThrough = 0;
+					LCDClear();
+				}
+		}while(input != 0);
+		UARTSendCharBlocking('\n');
+		}
 	}
 };
